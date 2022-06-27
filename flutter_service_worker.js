cb77cd87d0b26926ece3f29c2f50d0f2ae26b401
justin joseph <74088655+justincj2001@users.noreply.github.com>
2022-06-28 00:46:50 +0530

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -20,9 +20,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "1b4ce446c3d8bf073120b87500df4217",
-"/": "1b4ce446c3d8bf073120b87500df4217",
-"main.dart.js": "44a1be6f6bac9e0bd720da30c879a686",
+"index.html": "22da6fd4176cf224574d460e7449aec0",
+"/": "22da6fd4176cf224574d460e7449aec0",
+"main.dart.js": "4ea034260bbe991fee99e34616e0520a",
 "manifest.json": "b3c9ea2ff1a2817d37e6354eac9e7733",
 "version.json": "b6d7965eeca01658554c38779b8532a1"
 };
