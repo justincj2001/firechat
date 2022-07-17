d308d03bfab67302bf87f857815d5233457a550c
justin joseph <74088655+justincj2001@users.noreply.github.com>
2022-07-17 18:26:34 +0530

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,12 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "3d33bbfabdb89810305df17f953084ee",
-"assets/assets/ic_instagram.svg": "3faaddf43e4c4c3ade5d94e62ad9b645",
-"assets/assets/logo.svg": "1a30211d6dc5a9ae9e4c6bffd227d7aa",
+  "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
-"assets/NOTICES": "030c9d976a380f76d9dd7e46637fcc10",
+"assets/NOTICES": "e904acf0a2f167576f0099f8079bb818",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "canvaskit/canvaskit.js": "c2b4e5f3d7a3d82aed024e7249a78487",
 "canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba",
@@ -20,9 +18,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "b640059ac2e325193e325f35162f1344",
-"/": "b640059ac2e325193e325f35162f1344",
-"main.dart.js": "84b12d23bada16a6848e17c3e73289e6",
+"index.html": "03b3b5915d1a979657d17b2fa4f51fb0",
+"/": "03b3b5915d1a979657d17b2fa4f51fb0",
+"main.dart.js": "8612bcb7dcd5e5faa553a5cba76db323",
 "manifest.json": "b3c9ea2ff1a2817d37e6354eac9e7733",
 "version.json": "b6d7965eeca01658554c38779b8532a1"
 };
